3f3f3d0784e0ed66bb86dd317dbb4e9f84a033cf
Eswsx <957140834@qq.com>
2018-12-06 09:48:21 +0800

    1206

--- JAVA300/Object/src/cn/es/myCollection/MyArrayList.java
+++ JAVA300/Object/src/cn/es/myCollection/MyArrayList.java
@@ -10,15 +10,20 @@ public class MyArrayList {
 	
 	public MyArrayList(){
 //		value = new Object[16];
-		this(16);
+		this(10);
 	}
 	
 	public MyArrayList(int size){
+		rangCheck(size);
 		value = new Object[size];
 	}
 	
 	public int size(){
 		return size;
+	}
+	
+	public boolean isEmpty(){
+		return size == 0;
 	}
 	
 	public void add(Object obj){
@@ -35,16 +40,55 @@ public class MyArrayList {
 			value = newList;
 		}
 	}
+	
 	public Object get(int index){
-		if(index<0 || index>size-1){
+		rangCheck(index);
+		return value[index];
+	}
+	
+	public int indexOf(Object obj){
+		if(obj == null){
+			return -1;
+		}else{
+			for(int i=0;i<value.length;i++){
+				if(obj == value[i]){
+					return i;
+				}
+			}
+			return -1;
+		}
+	}
+	public int lastIndexOf(Object obj){
+		if(obj == null){
+			return -1;
+		}else{
+			for(int i=value.length;i>=0;i--){
+				if(obj == value[i]){
+					return i;
+				}
+			}
+			return -1;
+		}
+	}
+	
+	public Object set(int index,Object object){
+		rangCheck(index);
+		
+		Object old = value[index];
+		value[index] = object;
+		return old;
+	}
+	
+	public void rangCheck(int index){
+		if(size < 0){
 			try{
 				throw new Exception();//手动抛出异常
 			}catch(Exception e){
 				e.printStackTrace();
 			}
 		}
-		return value[index];
 	}
+	
 	public static void main(String[] args) {
 		MyArrayList list = new MyArrayList(2);
 		list.add("aaa");
