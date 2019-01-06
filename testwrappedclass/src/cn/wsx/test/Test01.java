f9cb0b9edcd0d31cb844b62ffe9f3877d23dbcb2
Eswsx <957140834@qq.com>
2019-01-06 09:26:09 +0800

    0106

---
+++ testwrappedclass/src/cn/wsx/test/Test01.java
@@ -0,0 +1,14 @@
+package cn.wsx.test;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+
+public class Test01 {
+	public static void main(String[] args) {
+		Integer i = new Integer(1000);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.toHexString(i));
+		Integer i2 = Integer.parseInt("234");
+		Integer i3 = new Integer("123");
+		System.out.println(i2+i3);
+	}
+}
