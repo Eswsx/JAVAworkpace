6df0d869ca7fd7feb37bc611ac914b882fe02329
Eswsx <957140834@qq.com>
2019-03-22 08:48:09 +0800

    032201

--- JAVA300/testDate/src/cn/wsx/test/VisualCanlendar.java
+++ JAVA300/testDate/src/cn/wsx/test/VisualCanlendar.java
@@ -6,6 +6,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Scanner;
 
 /**
  * 可视化日历程序
@@ -14,26 +15,36 @@ import java.util.GregorianCalendar;
  */
 public class VisualCanlendar {
 	public static void main(String[] args) {
-		String temp = "2030-5-10";
+		System.out.println("Please enter the date:XXXX-XX-XX");
+		Scanner sc = new Scanner(System.in);
+		String input  = sc.nextLine();
+		String temp = input;
+		
 		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 		try {
 			Date date = format.parse(temp);
 			Calendar calendar = new GregorianCalendar();
 			calendar.setTime(date);
-			
+			int day = calendar.get(Calendar.DATE);
 			calendar.set(Calendar.DATE, 1);
 			
-			System.out.println(calendar.get(calendar.DAY_OF_WEEK));
-			System.out.println(calendar.getActualMaximum(Calendar.DATE));
+			
+			int maxDate = calendar.getActualMaximum(Calendar.DATE);
 			
 			System.out.println("日\t一\t二\t三\t四\t五\t六");
-			int j=0;
-			for(int i=0;i<31;i++){
+			for(int i=0;i<calendar.get(calendar.DAY_OF_WEEK)-1;i++){
+				System.out.print("\t");
+			}
+			for(int i=1;i<maxDate;i++){
+				if(i == day){
+					System.out.print("*");
+				}
 					System.out.print(i+"\t");
-					j++;
-					if(j%7==0){
+					int w = calendar.get(Calendar.DAY_OF_WEEK);
+					if(w == Calendar.SATURDAY){
 						System.out.print("\n");
 					}
+					calendar.add(Calendar.DATE,1);
 				}
 		} catch (ParseException e) {
 			// TODO 自动生成的 catch 块
