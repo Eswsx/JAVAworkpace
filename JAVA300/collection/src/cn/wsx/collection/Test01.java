ee4b04987c305518e0ef64dd77d0cb1ebc06c862
Eswsx <957140834@qq.com>
2019-03-25 21:45:23 +0800

    032501

--- JAVA300/collection/src/cn/wsx/collection/Test01.java
+++ JAVA300/collection/src/cn/wsx/collection/Test01.java
@@ -7,6 +7,9 @@ import java.util.List;
 public class Test01 {
 	public static void main(String[] args) {
 		List list = new ArrayList();
+		//ArrayList:底层实现是数组。所以，查询快。修改，插入，删除慢。
+		//LinkList:底层实现是链表。所以，查询慢。修改，插入，删除快。
+		//Vector:线程安全，效率低。
 		
 		list.add("aaa");
 		list.add(new Date());
@@ -16,7 +19,7 @@ public class Test01 {
 		System.out.println(list.size());
 		
 		System.out.println(list.isEmpty());
-//		list.remove("aaa");
+//		list.remove(new Date());//hashcode 和 equals
 		System.out.println(list.size());
 		
 		List list2 = new ArrayList();
@@ -31,6 +34,7 @@ public class Test01 {
 		String str = (String)list.get(0);
 		System.out.println(str);
 		list.set(1,"aaaaba");
+		list.remove(0);
 	}
 }
 class Dog{
