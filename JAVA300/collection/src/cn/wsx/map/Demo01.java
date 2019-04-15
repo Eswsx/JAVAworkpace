ec8374c0cc0eb2a8eb557c61fe48f3fce51a9ecf
Eswsx <957140834@qq.com>
2019-04-15 22:13:48 +0800

    041502

--- JAVA300/collection/src/cn/wsx/map/Demo01.java
+++ JAVA300/collection/src/cn/wsx/map/Demo01.java
@@ -2,6 +2,7 @@ package cn.wsx.map;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * this is a cat and that is a mice and where is the food
@@ -26,5 +27,77 @@ public class Demo01 {
 		String[] strArray = str.split(" ");
 		//存储到map中
 		Map<String,Letter> letters = new HashMap<>();
+		for(String temp:strArray){
+			/*
+			 // 1.为所有key创建容器
+			if(!letters.containsKey(temp)){
+				Letter col = new Letter();
+				col.setCount(1);//第一次值存放容器
+				letters.put(temp,col);
+			}else{
+				//2.之后容器中存放对应value
+				Letter col = letters.get(temp);//直接使用容器
+				col.setCount(col.getCount()+1);
+			}
+			*/
+			Letter col = null;
+			if(null == (col = letters.get(temp))){
+				col = new Letter();
+				col.setCount(1);//第一次值存放容器
+				letters.put(temp,col);
+			}else{
+				//2.之后容器中存放对应value
+				col.setCount(col.getCount()+1);
+			}
+			
+		}
+		//输出MAP的值
+				Set<String> keys = letters.keySet();
+				for(String key:keys){
+					Letter col = letters.get(key);
+					System.out.println("字母:"+key+",次数"+ col.getCount());
+				}
+	}
+	public static void test01(){
+		String str = "this is a cat and that is a mice and where is the food";
+		//分割字符串
+		String[] strArray = str.split(" ");
+		//存储到map中
+		Map<String,Letter> letters = new HashMap<>();
+		/**
+		for(String temp:strArray){
+			**
+			 * 1.为所有key创建容器
+			 * 	 之后容器中存放对应value
+			 *
+			if(!letters.containsKey(temp)){
+				letters.put(temp,new Letter());
+			}
+		}
+		for(String temp:strArray){
+			**
+			 * 	容器中存放对应value
+			 *
+			Letter col = letters.get(temp);//直接使用容器
+			col.setCount(col.getCount()+1);
+			
+		}
+		*/	
+		for(String temp:strArray){
+			 // 1.为所有key创建容器
+			if(!letters.containsKey(temp)){
+				letters.put(temp,new Letter());
+			}
+			//2.之后容器中存放对应value
+			Letter col = letters.get(temp);//直接使用容器
+			col.setCount(col.getCount()+1);
+		}
+		//输出MAP的值
+		Set<String> keys = letters.keySet();
+		for(String key:keys){
+			Letter col = letters.get(key);
+			System.out.println("字母:"+key+",次数"+ col.getCount());
+		}
 	}
 }
+
